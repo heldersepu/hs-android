1b8c75d2ac85d8a3c6351112d3883dd02fbb65ca
heldersepu <heldersepu@34738d6d-0351-5e76-4d84-80caeacbd196>
2014-01-26 01:59:18 +0000

    Add WebView to the code

--- web.app/src/com/example/web/app/MainActivity.java
+++ web.app/src/com/example/web/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.web.app;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		WebView myWebView = (WebView) findViewById(R.id.webView1);
+		myWebView.loadUrl("file:///android_asset/waroftheworlds.html");
 	}
 
 	@Override
